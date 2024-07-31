1c6676c344546f2af9c399a46c4d479d53ebccb5
sysROM <gamecom00@outlook.com>
2024-07-31 12:17:00 +0800

    更新古董Patcher

--- QQNTPatcher/QQNTPatcher/main.cpp
+++ QQNTPatcher/QQNTPatcher/main.cpp
@@ -6,29 +6,41 @@
 #include <algorithm>
 #include <filesystem>
 
-//#ifdef _WIN64
+
+inline std::vector<uint8_t> arm64_sig = { 0x98,0x01,0x00,0x35,0xB2,0xED,0xFF,0x97}; //arm64
+inline std::vector<uint8_t> arm64_fixthisshit = { 0x98,0x01,0x00,0x35,0x20,0x00,0x80,0xD2 };
+
 inline std::vector<uint8_t> x64_sig = { 0x48,0x89,0xCE,0x48,0x8B,0x11,0x4C,0x8B,0x41,0x08,0x49,0x29,0xD0,0x48,0x8B,0x49,0x18,0xE8 }; //x64
 inline std::vector<uint8_t> x64_fixthisshit = { 0x48,0x89,0xCE,0x48,0x8B,0x11,0x4C,0x8B,0x41,0x08,0x49,0x29,0xD0,0x48,0x8B,0x49,0x18,0xB8,0x01,0x00,0x00,0x00 };
-//#else
+
 inline std::vector<uint8_t> x86_sig = { 0x89,0xCE,0x8B,0x01,0x8B,0x49,0x04,0x29,0xC1,0x51,0x50,0xFF,0x76,0x0C,0xE8 }; //x86
 inline std::vector<uint8_t> x86_fixthisshit = { 0x89,0xCE,0x8B,0x01,0x8B,0x49,0x04,0x29,0xC1,0x51,0x50,0xFF,0x76,0x0C,0xB8,0x01,0x00,0x00,0x00 };
-//#endif // !_WIN64
 
-struct ImageOptionalHeader {
-    uint16_t Magic;
-};
 
-bool Is64BitPE(const std::vector<uint8_t>& buffer)
-{
+int GetPEArch(const std::vector<uint8_t>& buffer) {
     if (buffer.size() >= sizeof(IMAGE_DOS_HEADER) + sizeof(IMAGE_NT_HEADERS32)) {
         const IMAGE_DOS_HEADER* dosHeader = reinterpret_cast<const IMAGE_DOS_HEADER*>(buffer.data());
-        const IMAGE_NT_HEADERS32* ntHeader32 = reinterpret_cast<const IMAGE_NT_HEADERS32*>(buffer.data() + dosHeader->e_lfanew);
-        if (dosHeader->e_magic == IMAGE_DOS_SIGNATURE && ntHeader32->Signature == IMAGE_NT_SIGNATURE) {
-            const ImageOptionalHeader* optionalHeader = reinterpret_cast<const ImageOptionalHeader*>(&ntHeader32->OptionalHeader);
-            return (optionalHeader->Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC); //64bit
+        if (dosHeader->e_magic == IMAGE_DOS_SIGNATURE) {
+            const IMAGE_NT_HEADERS* ntHeaders = reinterpret_cast<const IMAGE_NT_HEADERS*>(buffer.data() + dosHeader->e_lfanew);
+            if (ntHeaders->Signature == IMAGE_NT_SIGNATURE) {
+                if (ntHeaders->FileHeader.Machine == IMAGE_FILE_MACHINE_ARM64) {
+                    return 1;
+                }
+                else if(ntHeaders->FileHeader.Machine == IMAGE_FILE_MACHINE_AMD64)
+                {
+                    return 2;
+                }
+                else if(ntHeaders->FileHeader.Machine == IMAGE_FILE_MACHINE_I386)
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 4; //what the hell
+                }
+            }
         }
     }
-    return false; // 32bit
 }
 
 std::vector<uint8_t> ReadFile(const std::string& file_path) {
@@ -99,14 +111,33 @@ int main() {
         printf("[*] Backup At: %s\n", Filepath.c_str());
         std::vector<uint8_t> pe_file = ReadFile(Filepath);
 
-        if (Is64BitPE(pe_file)) {
-            printf("[+] PE file is 64bit!\n");
-            ScanSigAReplace(pe_file, x64_sig, x64_fixthisshit);
+        switch (GetPEArch(pe_file))
+        {
+            case 1:
+            {
+                printf("[+] PE file is ARM64!\n");
+                ScanSigAReplace(pe_file, arm64_sig, arm64_fixthisshit);
+                break;
+            }
+            case 2:
+            {
+                printf("[+] PE file is 64bit!\n");
+                ScanSigAReplace(pe_file, x64_sig, x64_fixthisshit);
+                break;
+            }
+            case 3:
+            {
+                printf("[+] PE file is 32bit!\n");
+                ScanSigAReplace(pe_file, x86_sig, x86_fixthisshit);
+                break;
+            }
+            case 4: 
+            {
+                printf("[-] Unknown architecture.");
+                break;
+            }
         }
-        else{
-            printf("[+] PE file is 32bit!\n");
-            ScanSigAReplace(pe_file, x86_sig, x86_fixthisshit);
-        }
+        
         std::ofstream output_file(savePath, std::ios::binary);
         output_file.write(reinterpret_cast<const char*>(pe_file.data()), pe_file.size());
         std::cout << "[+] Patched!\n" << std::endl;
